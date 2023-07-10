8debbc72e76e0210d4266ddf64c92e9ba66a76bb
Alexander Huemer <alexander.huemer@xx.vu>
2023-07-10 18:25:40 +0000

    dsl_file_offsets: dump sub-files

--- dsl_file_offsets.py
+++ dsl_file_offsets.py
@@ -3,24 +3,39 @@
 All requests employed in down stream loading use a one byte file identifier.
 """
 
+import os
 import sys
 
 START = 0x0a
 STEP = 0x0a
+DIR = "tmp"
+DUMP_FILES = False
 
 if len(sys.argv) != 2:
     print(f"Usage: {sys.argv[0]} <DSL file>")
     sys.exit(1)
 
+if DUMP_FILES:
+    try:
+        os.mkdir(DIR)
+    except FileExistsError:
+        pass
+
 with open(sys.argv[1], mode='rb') as dsl:
     content = dsl.read()
     for i in range(0x40):
-        offset = content[START+STEP*i:START+STEP*i+4]
-        length = content[START+STEP*i+4:START+STEP*i+8]
-        # print("offset:", offset, type(offset))
-        end = int.from_bytes(offset) + int.from_bytes(length)
-        # print(end, type(end))
+        offset = int.from_bytes(content[START+STEP*i:START+STEP*i+4])
+        length = int.from_bytes(content[START+STEP*i+4:START+STEP*i+8])
+        end = offset + length
         print(f"File 0x{i+1:02x}: "
-              f"Offset: 0x{offset.hex()} "
-              f"Length: 0x{length.hex()} "
+              f"Offset: 0x{offset:08x} "
+              f"Length: 0x{length:08x} "
               f"End: 0x{end:08x}")
+
+        if DUMP_FILES:
+            if offset == 0x0:
+                continue
+
+            with open(f"tmp/{i+1:02x}", mode='wb') as file:
+                subfile = content[offset:end]
+                file.write(subfile)
